f1888ce03a0d06e059a006bab4914da2218bb515
Bradley O'Connell <bradleyocon@gmail.com>
2022-01-11 00:00:25 -0500

    added restore and tool restore to build script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git pull
 set -ex
+dotnet restore
+dotnet tool restore
 dotnet build -c Release
 dotnet ef migrations add "Build-$(date +%s)" --no-build
 dotnet ef database update --no-build
