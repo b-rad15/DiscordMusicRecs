e2120d9cdb6082e4e7c75f24958d3a14209422dd
Bradley O'Connell <bradleyocon@gmail.com>
2022-01-17 16:09:37 -0500

    removed auto generation of migrations

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,6 +4,6 @@ set -ex
 dotnet restore
 dotnet tool restore
 dotnet build -c Release
-dotnet ef migrations add "Build-$(date +%s)" --no-build
+# dotnet ef migrations add "Build-$(date +%s)" --no-build
 dotnet ef database update --no-build
 dotnet run
